16df00c69ff054a16731084a1abb32417963e633
Avthar Sewrathan <avthar.s@gmail.com>
2017-11-14 23:18:17 -0500

    Added Button and input field

--- Bloom/App.js
+++ Bloom/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button, TouchableOpacity, 
+
+
+} from 'react-native';
 
 export default class App extends React.Component {
   render() {
@@ -12,17 +15,35 @@ export default class App extends React.Component {
   }
 }
 
+
+export class SessionButton extends React.Component {
+  render(){
+    return (
+          <View style = {{flexDirection: 'row'}}>
+            <TouchableOpacity style = {styles.sessionButton}>
+              <Text style = {{color: 'white', textAlign: 'center', fontSize: 16, }}> START </Text>
+            </TouchableOpacity>
+          </View>
+      );
+  }
+}
+
 // Test component for getting started
 export class Intro extends React.Component {
   render(){
     return (
       <View style = {styles.container}>
+
        <Text>Welcome to the Bloom Prototype</Text>
         <Text> Focus for 60s:</Text>
+        <Text> I want to focus on: </Text>
+        <TextInput style = {styles.taskInput} placeholder = "What do you want to focus on?" />
+        <SessionButton/>
       </View>
       );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -31,4 +52,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  sessionButton: {
+    margin: 10,
+    backgroundColor: 'blue',
+    flex: 1,
+    height: 60,
+    justifyContent: 'center',
+  },
+
+  taskInput: {
+    height: 40,
+    width: 250,
+    borderWidth: 0,
+    color: 'black',
+    borderRadius: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
 });
